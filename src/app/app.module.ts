8689ec4ba90abb2c1a723c11d1192a3c4649ba52
Jaziel Gutierrez <jazielgutierrez50@gmail.com>
2022-11-23 22:02:26 -0300

    Creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { MainComponent } from './component/main/main.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
